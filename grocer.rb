4d2abd4d57d22c86ea316905bc548619056f7e4d
Emily Wagner <emilywagner598@gmail.com>
2019-01-31 22:34:39 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,17 +1,6 @@
 require "pry"
 
 def consolidate_cart(cart)
-  # new_array = []
-  # cart.each do |element|
-  #   if new_array.include?(element) == false
-  #     element[element.keys.first][:count] = 1
-  #     new_array << element
-  #   else
-  #     new_array[new_array.index(element)][element.keys.first][:count] += 1
-  #   end
-  # end
-  # new_array
-
   new_hash = {}
 
   cart.each do |element|
@@ -26,7 +15,7 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+
 end
 
 def apply_clearance(cart)
