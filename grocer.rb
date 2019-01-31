49acece5e953a7e7aa622bca92f28218042fdbb6
Emily Wagner <emilywagner598@gmail.com>
2019-01-31 22:20:24 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -21,7 +21,7 @@ def consolidate_cart(cart)
       new_hash[element.keys.first] = element[element.keys.first]
       binding.pry
     else
-      new_hash[element.keys.first][:count]
+      new_hash[element.keys.first][:count] += 1
     end
   end
 
